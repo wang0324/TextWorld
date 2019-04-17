c00ce629b09fc54572937484ecec67b1ccf89481
Kevin Wang <kevinwang0324@gmail.com>
2019-04-17 13:37:29 -0700

    FIXED

--- src/Chicken.java
+++ src/Chicken.java
@@ -1,7 +1,7 @@
 public class Chicken extends Creature{
 
-    public Chicken(Room room, String name, String description) {
-        super(room, name, description);
+    public Chicken(Room r, String name, String description) {
+        super(r, name, description);
     }
 
     public void move() {
